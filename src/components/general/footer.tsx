d35ed7cace6d8552cfe4dbd172619f9e5d03f7f5
ivanleopoldo <ivanleopoldoc@gmail.com>
2026-05-07 13:14:02 +0800

    change

--- src/components/general/footer.tsx
+++ src/components/general/footer.tsx
@@ -1,7 +1,8 @@
 export function Footer() {
   return (
     <p className="px-12 font-major text-xs text-center text-wrap">
-      Revisit the website if you've changed your mind
+      Please revisit the website if you've changed your mind on or before May
+      30, 2026.
     </p>
   );
 }
